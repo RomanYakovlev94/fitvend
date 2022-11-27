0875b700daa6116d4b0bcdddc9b511fc9260c487
RomanYakovlev94 <80695880+RomanYakovlev94@users.noreply.github.com>
2022-11-27 22:09:40 +0300

    update

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
 const muteButton = document.getElementById("general__inside-moot");
 const muteVideo = document.getElementById("general__inside-video");
+const burgerNav = document.getElementById("navbar-toggler-icon");
+const togglerNav = document.getElementById("navbar-toggler");
 
 muteButton.addEventListener('click', () => {
     if(muteButton.classList.contains("general__inside-moot1")){
@@ -9,5 +11,14 @@ muteButton.addEventListener('click', () => {
         muteButton.classList.add("general__inside-moot1")
         muteVideo.removeAttribute("muted");
         muteVideo.muted = false;
+    };
+});
+
+togglerNav.addEventListener('click', () => {
+    if(togglerNav.classList.contains("collapsed")){
+        burgerNav.classList.remove("navbar-close");
+        console.log(1);
+    }else{
+        burgerNav.classList.add("navbar-close")
     };
 });
