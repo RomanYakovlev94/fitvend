41af238955b972b212356b15fd562bd5dac8cbb7
RomanYakovlev94 <80695880+RomanYakovlev94@users.noreply.github.com>
2022-11-29 22:13:12 +0300

    done slider

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,10 @@ const muteButton = document.getElementById("general__inside-moot");
 const muteVideo = document.getElementById("general__inside-video");
 const burgerNav = document.getElementById("navbar-toggler-icon");
 const togglerNav = document.getElementById("navbar-toggler");
+const playBtn = document.getElementsByClassName("swiper-slide_play");
+const videosArr = document.getElementsByClassName("swiper-slide_video");
+const soundBtn = document.getElementsByClassName("swiper-slide_sound")
+
 
 muteButton.addEventListener('click', () => {
     if(muteButton.classList.contains("general__inside-moot1")){
@@ -21,6 +25,36 @@ togglerNav.addEventListener('click', () => {
         burgerNav.classList.add("navbar-close")
     };
 });
+
+
+for (let i = 0; i < playBtn.length; i++) {
+    playBtn[i].addEventListener('click', () =>{
+        
+        for (let j =0; j < videosArr.length; j++){
+            videosArr[j].pause();
+            videosArr[j].muted = true;
+            playBtn[j].style.display = "block";
+        }
+        videosArr[i].play();
+        playBtn[i].style.display = "none";
+        soundBtn[i].style.display = "block";
+    })
+};
+
+for (let i = 0; i < soundBtn.length; i++) {
+    soundBtn[i].addEventListener('click', () =>{
+        videosArr[i].muted = false;
+        soundBtn[i].style.display = "none";
+    })
+};
+
+for (let g = 0; g < videosArr.length; g++) {
+    videosArr[g].addEventListener('click', () =>{
+        videosArr[g].pause();
+        soundBtn[g].style.display = "none";
+        playBtn[g].style.display = "block";
+    })
+};
 
 new Swiper (".mySwiper", {
     pagination: {
