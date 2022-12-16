45e1aaeb492d80c2c46fea40675af0ca2738c090
RomanYakovlev94 <80695880+RomanYakovlev94@users.noreply.github.com>
2022-12-16 13:48:52 +0300

    Create video page for mobile

--- src/js/script-all-pages.js
+++ src/js/script-all-pages.js
@@ -2,6 +2,9 @@ const burgerNav = document.getElementById("navbar-toggler-icon");
 const togglerNav = document.getElementById("navbar-toggler");
 const playVideoAbout = document.getElementById("about-video-item");
 const playVideoAboutBtn = document.getElementById("about-video-play");
+const playBtnVideo = document.getElementsByClassName("top-video__wrapper-play");
+const videosArrVideo = document.getElementsByClassName("top-video__wrapper-item");
+
 
 togglerNav.addEventListener('click', () => {
     if(togglerNav.classList.contains("collapsed")){
@@ -11,8 +14,17 @@ togglerNav.addEventListener('click', () => {
     };
 });
 
-playVideoAboutBtn.addEventListener('click', () => {
-    playVideoAboutBtn.style.display = "none";
-    playVideoAbout.play();
-    playVideoAbout.setAttribute("controls", "controls");
-});
+if(playVideoAboutBtn){
+    playVideoAboutBtn.addEventListener('click', () => {
+        playVideoAboutBtn.style.display = "none";
+        playVideoAbout.play();
+        playVideoAbout.setAttribute("controls", "controls");
+    });
+}
+for (let i = 0; i < playBtnVideo.length; i++) {
+    playBtnVideo[i].addEventListener('click', () =>{
+        videosArrVideo[i].play();
+        videosArrVideo[i].setAttribute("controls", "controls");
+        playBtnVideo[i].style.display = "none";
+    })
+};
